4549e652830a7dec282e9774bc4b2b8c8e6ac083
Carmen Barron <carmen.barron@gmail.com>
2016-11-30 15:50:56 -0500

    wrap method in module CoinFinder

--- max.rb
+++ max.rb
@@ -1,17 +1,20 @@
 
-cents = 15
-set_of_coins = [1, 5, 10, 25, 50]
+module CoinFinder
 
-def biggest_coin(cents, set_of_coins)
-  set_of_coins.each do |coin|
-    if cents >= coin
-      biggie = coin
-    else
-        "all done here = check out biggie"
-        p "cents is #{cents}"
-        p "biggie is #{biggie}"
+  def biggest_coin(cents, set_of_coins)
+      #accept the cents, and array as set of coins
+    biggie = set_of_coins[0]
+      #make variable available outside of do loop
+    set_of_coins.each do |coin|
+      if cents >= coin
+        biggie = coin
+      else
+          "all done here = check out biggie"
+      end
     end
+    return biggie.to_i
   end
+
 end
 
-biggest_coin(15, [1,5,10,25,50])
+# puts biggest_coin(15, [1,5,10,25])
